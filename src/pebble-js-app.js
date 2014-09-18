7dc740df7308361f48b1cf9a9d51a94f3e03bebd
John Sturdy <jcg.sturdy@gmail.com>
2014-09-18 22:40:46 +0100

    Start filling in with bits from guide

--- src/pebble-js-app.js
+++ src/pebble-js-app.js
@@ -1,1 +1,21 @@
-// Helper
+Pebble.addEventListener("ready",
+			function(e) {
+			    // all initialization to be done in here
+			    console.log("JavaScript app ready and running!");
+			}
+		       );
+
+Pebble.addEventListener("appmessage",
+			function(e) {
+			    console.log("Received message: " + e.payload);
+			}
+		       );
+
+"appKeys": {
+  "setSizes":  0,
+  "dataLine": 1
+}
+
+Pebble.sendAppMessage({"setSizes": "4 64" });
+Pebble.sendAppMessage({"dataLine": "Hello"});
+Pebble.sendAppMessage({"dataLine": "world"});
