730a758eccbb9a8f7b2efe8f83677eb13ad34bff
John Sturdy <jcg.sturdy@gmail.com>
2014-09-18 22:26:16 +0100

    Tidy up, and start on dynamic allocation of data

--- src/feature_menu_layer.c
+++ src/feature_menu_layer.c
@@ -6,88 +6,126 @@ static Window *window;
 // You have more control than with a simple menu layer
 static MenuLayer *menu_layer;
 
-
-
-static int current_icon = 0;
-
-
 // A callback is used to specify the amount of sections of menu items
 // With this, you can dynamically add and remove sections
-static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+static uint16_t
+menu_get_num_sections_callback(MenuLayer *menu_layer,
+			       void *data) 
+{
   return 1;
 }
+static char **lines = NULL;
+static int allocated_lines = 0;
+static int available_lines = 0;
+static char *bytes = NULL;
+static char *byte_fill = NULL;
+static char *last_char = NULL;
 
-static char *entries[] = {
-        "aleph", "bet", "gimel", "daleth", NULL
-};
-
-static char *entry_line(int index) {
-    return entries[index];
+static void
+add_line(char *line)
+{
+  int len = strlen(line);
+  if ((byte_fill + len) >= last_char) {
+    return;
+  }
+  strcpy(byte_fill, line);
+  lines[available_lines++] = byte_fill;
+  byte_fill += (len + 1);
 }
 
-static int count_entries() {
-    int i;
-    for (i = 0; entries[i] != NULL; i++) {
-//             nothing to do here
-    }
-    return i;
+static void
+allocate_storage(int alloc_lines, int alloc_bytes)
+{
+  lines = (char**)malloc(alloc_lines * sizeof(char*));
+  allocated_lines = alloc_lines;
+  byte_fill = bytes = (char*)malloc(alloc_bytes + 1);
+  last_char = bytes + alloc_bytes;
+}
+
+static char*
+entry_line(int index) 
+{
+  if (allocated_lines == 0) {
+    return "Nothing loaded";
+  }
+  return entries[index];
+}
+
+static int
+count_entries()
+{
+  int i;
+  if (allocated_lines == 0) {
+    return 1;			/* one fake entry to say nothing is loaded */
+  }
+  return available_lines;
 }
 
 // Each section has a number of items;  we use a callback to specify this
 // You can also dynamically add and remove items using this
-static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
-  switch (section_index) {
-    case 0:
-     return count_entries();
-    default:
-      return 0;
-  }
+static uint16_t
+menu_get_num_rows_callback(MenuLayer *menu_layer,
+			   uint16_t section_index,
+			   void *data)
+{
+  return count_entries();
 }
 
 // A callback is used to specify the height of the section header
-static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+static int16_t
+menu_get_header_height_callback(MenuLayer *menu_layer,
+				uint16_t section_index,
+				void *data)
+{
   // This is a define provided in pebble.h that you may use for the default height
+  /* todo: make this lower */
   return MENU_CELL_BASIC_HEADER_HEIGHT;
 }
 
 // Here we draw what each header is
-static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
-    menu_cell_basic_header_draw(ctx, cell_layer, "Some example items");
+static void
+menu_draw_header_callback(GContext* ctx,
+			  const Layer *cell_layer,
+			  uint16_t section_index,
+			  void *data)
+{
+  menu_cell_basic_header_draw(ctx, cell_layer, "Some example items");
 }
 
 // This is the menu item draw callback where you specify what each item should look like
-static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
-  // Determine which section we're going to draw in
-  switch (cell_index->section) {
-    case 0:
-      // Use the row to specify which item we'll draw
-      menu_cell_title_draw(ctx, cell_layer, entry_line(cell_index->row));
-          break;
-  }
+static void
+menu_draw_row_callback(GContext* ctx,
+		       const Layer *cell_layer,
+		       MenuIndex *cell_index,
+		       void *data)
+{
+  menu_cell_title_draw(ctx, cell_layer, entry_line(cell_index->row));
 }
 
 // Here we capture when a user selects a menu item
-void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
+static void
+menu_select_callback(MenuLayer *menu_layer,
+			  MenuIndex *cell_index,
+			  void *data)
+{
   // Use the row to specify which item will receive the select action
 #if 0
   switch (cell_index->row) {
     // This is the menu item with the cycling icon
-    case 1:
-      // Cycle the icon
-      current_icon = (current_icon + 1) % NUM_MENU_ICONS;
-      // After changing the icon, mark the layer to have it updated
-      layer_mark_dirty(menu_layer_get_layer(menu_layer));
-      break;
+  case 1:
+    // Cycle the icon
+    current_icon = (current_icon + 1) % NUM_MENU_ICONS;
+    // After changing the icon, mark the layer to have it updated
+    layer_mark_dirty(menu_layer_get_layer(menu_layer));
+    break;
   }
 #endif
 }
 
 // This initializes the menu upon window load
-void window_load(Window *window) {
-  // Here we load the bitmap assets
-  // resource_init_current_app must be called before all asset loading
- 
-
+static void 
+window_load(Window *window)
+{
   // Now we prepare to initialize the menu layer
   // We need the bounds to specify the menu layer's viewport size
   // In this case, it'll be the same as the window's
@@ -99,13 +137,13 @@ void window_load(Window *window) {
 
   // Set all the callbacks for the menu layer
   menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
-    .get_num_sections = menu_get_num_sections_callback,
-    .get_num_rows = menu_get_num_rows_callback,
-    .get_header_height = menu_get_header_height_callback,
-    .draw_header = menu_draw_header_callback,
-    .draw_row = menu_draw_row_callback,
-    .select_click = menu_select_callback,
-  });
+      .get_num_sections = menu_get_num_sections_callback,
+	.get_num_rows = menu_get_num_rows_callback,
+	.get_header_height = menu_get_header_height_callback,
+	.draw_header = menu_draw_header_callback,
+	.draw_row = menu_draw_row_callback,
+	.select_click = menu_select_callback,
+	});
 
   // Bind the menu layer's click config provider to the window for interactivity
   menu_layer_set_click_config_onto_window(menu_layer, window);
@@ -114,23 +152,29 @@ void window_load(Window *window) {
   layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
 }
 
-void window_unload(Window *window) {
+static void
+window_unload(Window *window)
+{
   // Destroy the menu layer
   menu_layer_destroy(menu_layer);
-
-  
 }
 
-int main(void) {
+int
+main(void)
+{
   window = window_create();
 
-  // Setup the window handlers
   window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
+      .load = window_load,
+	.unload = window_unload,
+	});
 
   window_stack_push(window, true /* Animated */);
+
+  allocate_storage(3, 16);
+  add_line("foo");
+  add_line("bar");
+  add_line("baz");
 
   app_event_loop();
 
