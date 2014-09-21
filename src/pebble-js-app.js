e09659983ae39459b35337adaed270e2ec08046b
John Sturdy <jcg.sturdy@gmail.com>
2014-09-21 22:02:20 +0100

    add sending of lines

--- src/pebble-js-app.js
+++ src/pebble-js-app.js
@@ -1,23 +1,64 @@
-Pebble.addEventListener("ready",
-			function(e) {
-			    // all initialization to be done in here
-			    // todo: load lines from file
-			    console.log("JavaScript app ready and running!");
-			}
-		       );
+var next_line = -1;
+var char_count = 1024;
+var line_count = 16;
 
-Pebble.addEventListener("appmessage",
-			function(e) {
-			    console.log("Received message: " + e.payload);
-			    // todo: switch on message type, which can be a request for data, or a chagne of status
-			}
-		       );
-
-"appKeys": {
-  "setSizes":  0,
-  "dataLine": 1
+function initializer(e) 
+{
+    // all initialization to be done in here
+    // todo: load lines from file
+    console.log("JavaScript app ready and running!");
 }
 
-Pebble.sendAppMessage({"setSizes": "4 64" });
-Pebble.sendAppMessage({"dataLine": "Hello"});
-Pebble.sendAppMessage({"dataLine": "world"});
+function responder(e) {
+    console.log("Received message: " + e.payload);
+    // todo: switch on message type, which can be a request for data, or a change of status
+    if (next_line < 0) {
+	Pebble.sendAppMessage({"setSpace": char_count, "setLines", line_count})
+	next_line = 0;
+    } else {
+	if (lines[next_line]) {
+	    sendLine(next_line++);
+	}
+    }
+}
+
+Pebble.addEventListener("ready", initializer);
+Pebble.addEventListener("appmessage", responder);
+
+var lines = [
+    "a haon",
+    "a do",
+    "a tri",
+    "a ceathair",
+    "a cuig",
+    "a se",
+    "a seacht",
+    "a hocht",
+    "a naoi",
+    "deich",
+    "aon deag",
+    "do dheag"
+];
+
+function sendLineSucceeded(e)
+{
+    console.log("Successfully delivered message with transactionId="
+		+ e.data.transactionId);
+};
+
+function sendLineFailed(e)
+{
+    console.log("Unable to deliver message with transactionId="
+		+ e.data.transactionId
+		+ " Error is: " + e.error.message);
+};
+
+function sendLine(line_index)
+{
+    send_data =  { "lineIndex": line_index,
+		   "lineText": lines[line_index] }
+    var transactionId = Pebble.sendAppMessage(send_data,
+					      sendLineSucceeded,
+					      sendLineFailed
+					     );
+};
